686e86013d1860bd4daf474db0cfa6b001ba75fe
Matt Stillwell <stillwell006@gmail.com>
2021-12-01 18:38:15 -0500

    ✨ Add solution for day 1 part 2

---
+++ day1/Day1Part2.swift
@@ -0,0 +1,45 @@
+#!/usr/bin/swift
+
+func countGroups3LargerThanPrevious(nums: [Int]) -> Int{
+    var numLarger = 0
+    var groupNum = 0
+    var previousGroupSum = 0
+    var currentGroupSum = 0
+    
+    for (index, _) in nums.enumerated() {
+        if index > nums.count - 3 {
+            break;
+        }
+        if groupNum == 0 {
+            previousGroupSum = sumNext3(nums: nums, index: index)
+        } else {
+            currentGroupSum = sumNext3(nums: nums, index: index)
+            if currentGroupSum > previousGroupSum {
+                numLarger += 1;
+            }
+            previousGroupSum = currentGroupSum
+        }
+        groupNum += 1;
+    }
+    return numLarger
+}
+
+func sumNext3(nums: [Int], index: Int) -> Int {
+    return nums[index] + nums[index + 1] + nums[index + 2]
+}
+
+let sampleInput = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263
+]
+
+let answer = countGroups3LargerThanPrevious(nums: sampleInput)
+print(answer)
