4f707c40b8f86d2d3f3fcab12b390194400dbd28
Matt Stillwell <stillwell006@gmail.com>
2021-12-08 22:12:33 -0500

    ✨ Seperated file read logic

--- day3/Day3Part1.swift
+++ day3/Day3Part1.swift
@@ -14,22 +14,20 @@ class Count {
     }
 }
 
-func calculatePowerConsumption(filename: String) -> Int {
-    // Read the contents of the specified file
-    let contents = try! String(contentsOfFile: filename)
-
-    // Split the file into separate lines
-    let lines = contents.split(separator:"\n")
-
+func calculatePowerConsumption(_ input: [String]) -> Int {
+    if input.count == 0 {
+        return 0
+    }
+    
     var colFreq: [Count] = []
-    let numCols = lines[0].count
+    let numCols = input[0].count
 
     for _ in 0...numCols-1 {
         colFreq.append(Count())
     }
 
     // Iterate over each line and print the line
-    for line in lines {
+    for line in input {
         // print("\(line)")
         for col in 0...numCols-1 {
             let digit = Array(line)[col]
@@ -58,6 +56,13 @@ func calculatePowerConsumption(filename: String) -> Int {
     return binary2decimal(gamma) * binary2decimal(epsilon)
 }
 
+func readFile(filename: String) -> [String]? {
+    guard let contents = try? String(contentsOfFile: filename) else {
+        return nil
+    }
+    return contents.split(separator: "\n").map(String.init)
+}
+
 func binary2decimal(_ binary: String) -> Int {
     var amount: Double = 0
     var count: Double = 0
@@ -72,19 +77,26 @@ func binary2decimal(_ binary: String) -> Int {
 
 func main() {
     runTests()
-    let answer = calculatePowerConsumption(filename: "sampleInput2.txt")
+    let sampleInput2 = readFile(filename: "sampleInput2.txt")!
+    let answer = calculatePowerConsumption(sampleInput2)
     print(answer)
 }
 
 func runTests() {
+    assert(readFile(filename: "nonexistentInput.txt") == nil)
+    assert(readFile(filename: "garbageInput.txt")! == ["0000","0000","0000"])
+
     assert(binary2decimal("0") == 0)
     assert(binary2decimal("1") == 1)
     assert(binary2decimal("10") == 2)
     assert(binary2decimal("010") == 2)
     assert(binary2decimal("100") == 4)
 
-    assert(calculatePowerConsumption(filename: "garbageInput.txt") == 0)
-    assert(calculatePowerConsumption(filename: "sampleInput.txt") == 198)
+    let garbageInput = readFile(filename: "garbageInput.txt")!
+    let sampleInput = readFile(filename: "sampleInput.txt")!
+    assert(calculatePowerConsumption(garbageInput) == 0)
+    assert(calculatePowerConsumption(sampleInput) == 198)
+    assert(calculatePowerConsumption([]) == 0)
 }
 
 
